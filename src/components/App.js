589d81adc22328e1e2ac5f8d1e64e3a47bb7c4fa
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-24 16:14:23 +0300

    add unshplash api query

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import axios from "axios";
+
 import { SearchBar } from "./SearchBar";
 
 export class App extends React.Component {
   onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID 6cc9f8f58fbf3cacfd8dbe8f1812de9d6201f8670c3433cae932f3d8c25ac0d4"
+      }
+    });
     console.log({ term });
   }
   render() {
