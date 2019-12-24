7c210f341deb3eabdf1647dc0feea132d393f504
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-24 13:48:45 +0300

    add search bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import { SearchBar } from "./SearchBar";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div className={"ui container"} style={{ marginTop: "50px" }}>
+      <SearchBar />
+    </div>
+  );
 };
